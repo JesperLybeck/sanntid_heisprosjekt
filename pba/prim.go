b23e2f8d97c442720d22ded40ec7c13bc86e8851
Tora Bøckman <Tobockman@gmail.com>
2025-03-02 15:13:20 +0100

    -

--- pba/prim.go
+++ pba/prim.go
@@ -27,44 +27,46 @@ func Primary(ID string) {
 			ticker := time.NewTicker(2 * time.Second)
 
 			for {
-				select {
-				case p := <-peersRX:
-					if fsm.BackupID == "" && len(p.Peers) > 1 {
-						for i := 0; i < len(p.Peers); i++ {
-							if p.Peers[i] != ID {
-								fsm.BackupID = p.Peers[i]
-							}
-						}
-					}
-
-					fmt.Println("Peer update", p.Peers)
-					fmt.Println("New", p.New)
-					fmt.Println("Lost", p.Lost)
-					for i := 0; i < len(p.Lost); i++ {
-						if p.Lost[i] == fsm.BackupID {
-							println("Backup lost")
-							for j := 0; j < len(p.Peers); j++ {
-								if p.Peers[j] != fsm.PrimaryID {
-									fsm.BackupID = p.Peers[j]
-								} else {
-									fsm.BackupID = ""
+				if ID == fsm.PrimaryID {
+					select {
+					case p := <-peersRX:
+						if fsm.BackupID == "" && len(p.Peers) > 1 {
+							for i := 0; i < len(p.Peers); i++ {
+								if p.Peers[i] != ID {
+									fsm.BackupID = p.Peers[i]
 								}
 							}
 						}
+
+						fmt.Println("Peer update", p.Peers)
+						fmt.Println("New", p.New)
+						fmt.Println("Lost", p.Lost)
+						for i := 0; i < len(p.Lost); i++ {
+							if p.Lost[i] == fsm.BackupID {
+								println("Backup lost")
+								for j := 0; j < len(p.Peers); j++ {
+									if p.Peers[j] != fsm.PrimaryID {
+										fsm.BackupID = p.Peers[j]
+									} else {
+										fsm.BackupID = ""
+									}
+								}
+							}
+						}
+
+					case <-ticker.C:
+						fmt.Println("sending primary status version: ", fsm.Version)
+
+						statusTX <- fsm.Status{TransmitterID: ID, ReceiverID: fsm.BackupID, Orders: [4][3]bool{{false, false, false}, {false, false, false}, {false, false, false}, {false, false, false}}, Version: fsm.Version}
+
+						/*
+							case a <-orderRX:
+								Hall assignment
+								change ID in order
+								orderTX <- a
+
+						*/
 					}
-
-				case <-ticker.C:
-					fmt.Println("sending primary status version: ", fsm.Version)
-
-					statusTX <- fsm.Status{TransmitterID: ID, ReceiverID: fsm.BackupID, Orders: [4][3]bool{{false, false, false}, {false, false, false}, {false, false, false}, {false, false, false}}, Version: fsm.Version}
-
-					/*
-						case a <-orderRX:
-							Hall assignment
-							change ID in order
-							orderTX <- a
-
-					*/
 				}
 			}
 		}
